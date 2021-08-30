65b5216dfe56aabcb936ea99cf048965eaa0e113
attichouse <77102070+attichouse@users.noreply.github.com>
2021-08-30 18:17:59 +0200

    Initial commit

---
+++ Test/test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test {
+
+    @Test
+    void shouldConvert1toI(){
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int i) {
+        return "I";
+    }
+}
